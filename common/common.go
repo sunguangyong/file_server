8c25ce921a6a83a71b8a75556a7e1106ab43c565
sunguangyong <sunguangyong@yplsec.cn>
2020-05-26 18:39:52 +0800

    替换文件名里的所有(

--- common/common.go
+++ common/common.go
@@ -3,14 +3,14 @@ package common
 import (
     "file_server/config"
     "file_server/storage"
-    "os"
-    "io"
     "fmt"
-    "time"
+    "io"
     "math/rand"
-    "strings"
     "mime/multipart"
     "net/http"
+    "os"
+    "strings"
+    "time"
 )
 
 
@@ -59,6 +59,9 @@ func GenFileName(orig_file_name string) (file_name string) {
     items[len(items)-2] = fmt.Sprintf( "%s_%d_%d", strings.Replace(items[len(items)-2], " ", "", -1), time.Now().Unix(), GenRandomInt())
 
     file_name = strings.Join(items, ".")
+    file_name = strings.Replace(file_name,"(","_",-1) // bash: 未预期的符号 `(' 附近有语法错误
+
+    file_name = strings.Replace(file_name,")","_",-1)
     return
 }
 
