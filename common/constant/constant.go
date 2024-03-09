e81382629f92612a45103c98caed907c131f1959
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2024-03-09 12:48:02 +0800

    fix

--- common/constant/constant.go
+++ common/constant/constant.go
@@ -1,9 +1,8 @@
 package constant
 
 const (
-	LISEN_HOST = "0.0.0.0:8989"
 	STORAGE_IP = "0.0.0.0"
-	STATIC_URL = "http://121.91.171.184:8989"
+	STATIC_URL = "http://82.157.202.19:8888"
 	STATIC_DIR = "/Users/sunguangyong/workspace"
 	DB_ADDRESS = "root:qIlRn)muX83d@tcp(82.156.56.237:3306)/telegram_bot?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
 )
