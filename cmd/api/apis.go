98a81dd5a95eda5ca44a24facd1d3656ee8292d8
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2024-03-09 11:51:52 +0800

    fix

--- cmd/api/apis.go
+++ cmd/api/apis.go
@@ -12,7 +12,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "/Users/sunguangyong/workspace/src/self/file_server/cmd/api/etc/apis.yaml", "the config file")
+var configFile = flag.String("f", "etc/apis.yaml", "the config file")
 
 func main() {
 	flag.Parse()
