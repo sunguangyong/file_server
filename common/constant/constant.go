67c8df56990c6c8449f2ebfa916d1df7cbb0cafd
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2024-03-09 18:57:01 +0800

    fix

--- common/constant/constant.go
+++ common/constant/constant.go
@@ -3,7 +3,7 @@ package constant
 const (
 	STORAGE_IP = "0.0.0.0"
 	STATIC_URL = "http://43.133.42.227:8888"
-	STATIC_DIR = "/Users/sunguangyong/workspace"
+	STATIC_DIR = "/home/files/workspace"
 	DB_ADDRESS = "root:OGY0ZGY2OTJjZTQzMWEyZGZk@tcp(43.134.109." +
 		"161:13306)/allinoneagain?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
 )
