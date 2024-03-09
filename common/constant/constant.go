d608484d9ef1403fa177f864ac109ab841541a46
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2024-03-09 18:51:56 +0800

    fix

--- common/constant/constant.go
+++ common/constant/constant.go
@@ -4,5 +4,6 @@ const (
 	STORAGE_IP = "0.0.0.0"
 	STATIC_URL = "http://43.134.109.161:8888"
 	STATIC_DIR = "/Users/sunguangyong/workspace"
-	DB_ADDRESS = "root:OGY0ZGY2OTJjZTQzMWEyZGZk(43.134.109.161:13306)/allinoneagain?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
+	DB_ADDRESS = "root:OGY0ZGY2OTJjZTQzMWEyZGZk@tcp(43.134.109." +
+		"161:13306)/allinoneagain?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
 )
